ad86b1592702b39a7f82962ec2f2e43a9630a886
Andy Doan <adoan@nstrading.com>
2024-09-04 16:39:12 -0400

    Fix vscode.nix config to work with extensions that aren't in the standard vscode-extensions package.

--- vscode/vscode.nix
+++ vscode/vscode.nix
@@ -1,13 +1,14 @@
-# vscode-config.nix
-
 { config, pkgs, ... }:
 
 {
   programs.vscode = {
     enable = true;
+    enableUpdateCheck = true;
+    enableExtensionUpdateCheck = true;
     package = pkgs.vscode;
+    mutableExtensionsDir = true;
 
-    settings = {
+    userSettings = {
       "diffEditor.renderSideBySide" = false;
       "explorer.confirmDelete" = false;
       "editor.fontFamily" = "JetBrainsMono Nerd Font";
@@ -40,3 +41,36 @@
       }
     ];
 
+    extensions = with pkgs.vscode-extensions; [
+      ms-python.python
+      visualstudioexptteam.vscodeintellicode
+      github.github-vscode-theme
+      tamasfe.even-better-toml
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "pylint";
+        publisher = "ms-python";
+        version = "2023.10.1";
+        sha256 = "1tI5u33c5CHFQxkJZ/OxW3ZA5qPr4RoCIf6dqIMPykQ=";
+      }
+      {
+        name = "remote-ssh";
+        publisher = "ms-vscode-remote";
+        version = "0.113.1";
+        sha256 = "/tyyjf3fquUmjdEX7Gyt3MChzn1qMbijyej8Lskt6So=";
+      }
+      {
+        "name" = "vscode-todo-highlight";
+        "version" = "2.0.8";
+        "publisher" = "jgclark";
+        sha256 = "/CctaLcG+dA2Cf69/ACeDKdRLsu/VUGbAxUbyhI0VyA=";
+      }
+      {
+        "name" = "prettier-vscode";
+        "version" = "11.0.0";
+        "publisher" = "esbenp";
+        sha256 = "pNjkJhof19cuK0PsXJ/Q/Zb2H7eoIkfXJMLZJ4lDn7k=";
+      }
+    ];
+  };
+}
